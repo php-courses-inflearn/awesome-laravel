38ae7aee612df85f70dc8e2b7a936eeb44b47924
Sangwoo Jeong <pronist@naver.com>
2022-07-23 01:06:15 +0900

    Add mail:send command scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->command('mail:send --queue=emails')
+            ->weeklyOn(Schedule::MONDAY, '8:00');
     }
 
     /**
