31a50ff542bcd37c8351aa4d71d7842e51c9b5ee
pronist <pronist@naver.com>
2022-11-10 01:31:28 +0900

    Add channel tests

---
+++ tests/Feature/Broadcasting/UserChannelTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature\Broadcasting;
+
+use App\Broadcasting\UserChannel;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserChannelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * UserChannel 테스트
+     *
+     * @return void
+     */
+    public function testUserChannel()
+    {
+        $user = $this->user();
+
+        $userChannel = new UserChannel();
+
+        $this->assertTrue(
+            $userChannel->join($user, $user->id)
+        );
+        $this->assertFalse(
+            $userChannel->join($user, 1000)
+        );
+    }
+
+    /**
+     * User
+     *
+     * @return \App\Models\User
+     */
+    private function user()
+    {
+        $factory = User::factory();
+
+        return $factory->create();
+    }
+}
