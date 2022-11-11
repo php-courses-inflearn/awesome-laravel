4d65a15ee812328cfd9322d7e15deb7224c02a40
pronist <pronist@naver.com>
2022-11-11 20:10:21 +0900

    Refactoring

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -15,11 +15,19 @@ class WelcomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        /** @var \App\Models\User $user */
         $user = $request->user();
 
-        $posts = $user->subscriptions()->exists()
-            ? $user->subscriptions()->with('posts', fn ($query) => $query->limit(20))->get()->feed()
-            : Post::latest()->limit(20)->get();
+        if ($user->subscriptions()->exists()) {
+            /** @var \App\Collections\BlogCollection $subscriptions */
+            $subscriptions = $user->subscriptions()
+                ->with('posts', fn ($query) => $query->limit(20))
+                ->get();
+
+            $posts = $subscriptions->feed();
+        } else {
+            $posts = Post::latest()->limit(20)->get();
+        }
 
         return view('welcome', [
             'posts' => $posts->paginate(5, $request->page ?? 1),
