1e3b3afe7155d151906bf190d6ec013407aa0ba9
pronist <pronist@naver.com>
2023-02-18 00:13:22 +0900

    Add validate method in password rule

--- app/Rules/Password.php
+++ app/Rules/Password.php
@@ -3,16 +3,16 @@
 namespace App\Rules;
 
 use Closure;
-use Illuminate\Contracts\Validation\InvokableRule;
+use Illuminate\Contracts\Validation\ValidationRule;
 
-class Password implements InvokableRule
+class Password implements ValidationRule
 {
     /**
      * Run the validation rule.
      *
      * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
      */
-    public function __invoke(string $attribute, mixed $value, Closure $fail): void
+    public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         if (! preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/', $value)) {
             //$fail('The :attribute must contain at least one lowercase letter, one uppercase letter, one number, and one special character.');
