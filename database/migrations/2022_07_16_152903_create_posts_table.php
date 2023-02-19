ec6203795b1a4d6330fd8daed2d8a6670f623e3f
pronist <pronist@naver.com>
2023-02-20 02:26:34 +0900

    wip

--- database/migrations/2022_07_16_152903_create_posts_table.php
+++ database/migrations/2022_07_16_152903_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
                 ->constrained()
                 ->cascadeOnDelete();
             $table->string('title');
-            $table->string('content');
+            $table->text('content');
             $table->timestamps();
         });
     }
